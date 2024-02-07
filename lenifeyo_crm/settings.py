aad3d83b6aefc68f94556f9c7b2d495a19e2eb98
jeas560 <jeas560@gmail.com>
2024-02-07 15:56:47 -0300

    first record model and fixing debug mode settings

--- lenifeyo_crm/settings.py
+++ lenifeyo_crm/settings.py
@@ -15,7 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG") == "True"
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
