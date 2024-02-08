c87161c91c8c4d2625089fd31ed10c0ce8e2ae3b
jeas560 <jeas560@gmail.com>
2024-02-08 15:10:00 -0300

    Adding table for records and initial view version for each record

--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -17,7 +20,7 @@ def home(request):
             messages.error(request, "Not Welcome")
             return redirect("home")
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"records": records})
 
 
 # def login_user(request):
@@ -28,3 +31,12 @@ def logout_user(request):
     logout(request)
     messages.success(request, "See ya")
     return redirect("home")
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+    else:
+        messages.error(request, "You must be logged to view that page!")
+        return redirect("home")
